448a43a1252d9ee06a98d2db9c53e06b876e7646
ShemSkillman <43577362+ShemSkillman@users.noreply.github.com>
2019-10-09 20:02:36 +0100

    Tank movement
    Completed tasks:
    - Created player tank
    - Added 3rd person camera
    - Movement script
    - Environment with obstacles
    - Rotating camera control

---
+++ TankShooter3D/Assets/Scripts/Control/LookAt.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace TankShooter.Control
+{
+    public class LookAt : MonoBehaviour
+    {
+        [SerializeField] float lookSensitivity = 10f;
+
+        private void Update()
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * lookSensitivity * Input.GetAxis("Mouse X"));
+        }
+    }
+}
+
---
+++ TankShooter3D/Assets/Scripts/Control/Mover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace TankShooter.Control
+{
+    public class Mover : MonoBehaviour
+    {
+        [SerializeField] float moveSpeed = 10f;
+        [SerializeField] float rotationSpeed = 5f;
+
+        private void Update()
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed, Space.Self);
+            transform.Rotate(Vector3.up * Time.deltaTime * Input.GetAxis("Horizontal") * rotationSpeed);
+        }
+    }
+}
