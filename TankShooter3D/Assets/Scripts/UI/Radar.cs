ced758bbff9d74def7aa4bf7c0268e9fa8a36d45
ShemSkillman <43577362+ShemSkillman@users.noreply.github.com>
2019-12-19 08:55:42 +0000

    Radar sprites
    Completed tasks:
    - Created radar sprites for player, enemy tanks and projectiles in GIMP
    - Started writing radar script with basic enemy collider detection

--- TankShooter3D/Assets/Scripts/UI/Radar.cs
+++ TankShooter3D/Assets/Scripts/UI/Radar.cs
@@ -1,23 +1,79 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine.UI;
+using UnityEngine;
+using TankShooter.Core;
+using System.Collections;
+using TankShooter.Movement;
+using System.Collections.Generic;
 
-namespace TankShooter.UI
+public class Radar : MonoBehaviour
 {
-    public class Radar : MonoBehaviour
+    [SerializeField] Image projectilePrefab;
+    [SerializeField] Image enemyPrefab;
+    [SerializeField] Image playerPrefab;
+    [SerializeField] int radarPingFrequencyPerSecond = 1;
+    [SerializeField] float radarRange = 100;
+
+    RadarEntity radarPlayer;
+    List<RadarEntity> radarEnemies;
+    List<RadarEntity> radarProjectiles;
+
+    LayerMask enemyMask;
+    LayerMask projectileMask;
+
+    RectTransform canvasRect;
+    Transform playerTransform;
+
+    private void Awake()
     {
-        [SerializeField] RectTransform radarTank;
-        [SerializeField] Transform tank;
+        GameObject playerTank = GameObject.FindWithTag("Player");
+        playerTransform = playerTank.GetComponentInChildren<TankMove>().transform;
 
-        [SerializeField] RectTransform radarTurret;
-        [SerializeField] Transform turret;
+        enemyMask = LayerMask.GetMask("Enemy detection");
 
-        private void Update()
+        canvasRect = GetComponent<RectTransform>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(RadarUpdate());
+
+        radarEnemies = new List<RadarEntity>();
+        radarProjectiles = new List<RadarEntity>();
+    }
+
+    IEnumerator RadarUpdate()
+    {
+        Image playerMarker = Instantiate(playerPrefab, canvasRect.transform);
+        radarPlayer = new RadarEntity(playerMarker, playerTransform);
+
+        print(canvasRect.sizeDelta);
+
+        while (true)
         {
-            if (tank == null) return;
+            Collider[] enemyColliders = Physics.OverlapSphere(playerTransform.position, radarRange, enemyMask);
 
-            radarTank.rotation = new Quaternion(radarTank.rotation.x, radarTank.rotation.y, -tank.rotation.y, tank.rotation.w);
-            radarTurret.rotation = new Quaternion(radarTurret.rotation.x, radarTurret.rotation.y, -turret.rotation.y, turret.rotation.w);            
+            foreach(Collider enemy in enemyColliders)
+            {
+                Transform enemyTransfom = enemy.GetComponentInParent<TankMove>().transform;
+                Image enemyMarker = Instantiate(enemyPrefab, canvasRect.transform);
+
+                RadarEntity radarEnemy = new RadarEntity(enemyMarker, enemyTransfom);
+                radarEnemies.Add(radarEnemy);
+            }
+
+            yield return new WaitForSeconds(1 / radarPingFrequencyPerSecond);
+        }
+    }
+
+    private class RadarEntity
+    {
+        public Image Marker { get; }
+        public Transform WorldTransform { get;}
+
+        public RadarEntity(Image marker, Transform worldTransform)
+        {
+            Marker = marker;
+            WorldTransform = worldTransform;
         }
     }
 }
-
---
+++ TankShooter3D/Assets/Scripts/UI/TankDiagram.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace TankShooter.UI
+{
+    public class TankDiagram : MonoBehaviour
+    {
+        [SerializeField] RectTransform radarTank;
+        [SerializeField] Transform tank;
+
+        [SerializeField] RectTransform radarTurret;
+        [SerializeField] Transform turret;
+
+        private void Update()
+        {
+            if (tank == null) return;
+
+            radarTank.rotation = new Quaternion(radarTank.rotation.x, radarTank.rotation.y, -tank.rotation.y, tank.rotation.w);
+            radarTurret.rotation = new Quaternion(radarTurret.rotation.x, radarTurret.rotation.y, -turret.rotation.y, turret.rotation.w);            
+        }
+    }
+}
+
